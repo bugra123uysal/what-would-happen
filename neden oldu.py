5960ca567a601ff42f0ef0b559fa5122b182c5f9
buğra uysal <uysalbugra134@gmail.com>
2025-10-22 23:39:58 +0300

    There is information about stocks such as pe, beta, volume etc. and analysis has begun.

--- neden oldu.py
+++ neden oldu.py
@@ -3,21 +3,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import requests 
 
-url=pd.read_excel("C:\\Users\\buğra\\Desktop\\hisseler_google_finance (1).xlsx")
+url=pd.read_excel("C:\\Users\\buğra\\Desktop\\ne-olur\\hisseler_google_finance.xlsx")
 
 
 print(url.columns)
 """ aapl  """
-aapl=url['AAPL']
 tarih=url['Date']
 
-hkodu={'AAPL', 'MSFT', 'AMZN', 'NVDA', 'META', 'GOOG', 'TSLA', 'PEP',
-       'COST', 'AVGO', 'ADBE', 'NFLX'}
+hkodu={'Close', 'MSFT', 'AMZN', 'NVDA', 'META', 'GOOG', 'TSLA', 'PEP',
+       'COST', 'AVGO', 'ADBE', 'NFLX', 'JPM', 'V', 'JNJ', 'UNH', 'XOM', 'NEE',
+       'WMT', 'NKE', 'CAT', 'LMT', 'ASML', 'TSM', 'GS', 'PFE', 'SBUX', 'AMD'}
 for hk in hkodu:
    ilk=url[hk].iloc[0]
    son=url[hk].iloc[-4]
    
-   yüzdesel=((son - ilk)/ilk )*100
+   yüzdesel=((son -ilk)/ilk)*100
+ 
    print(f"{hk}: {yüzdesel}")
 
    
@@ -25,5 +26,9 @@ for hk in hkodu:
    plt.title(f"{hk} hisse senedi 2020-2025")
    plt.show()
 
-
+NVDA: 2897.17
+AVGO: 989.73
+TSLA: 1431.76
+TSM: 407.51
+AMD: 374.70
 
