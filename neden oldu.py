9a39d6a2f9ae6b887c07f4b0627bbb46160532fd
buğra uysal <uysalbugra134@gmail.com>
2025-10-29 00:26:27 +0300

    Added return calculations and charts; 50/200-day moving averages not yet working

--- neden oldu.py
+++ neden oldu.py
@@ -5,30 +5,52 @@ import requests
 
 url=pd.read_excel("C:\\Users\\buğra\\Desktop\\ne-olur\\hisseler_google_finance.xlsx")
 
+""" 
 
-print(url.columns)
+hkodu=['Close', 'MSFT', 'AMZN', 'NVDA', 'META', 'GOOG', 'TSLA', 'PEP',
+       'COST', 'AVGO', 'ADBE', 'NFLX', 'JPM', 'V', 'JNJ', 'UNH', 'XOM', 'NEE',
+       'WMT', 'NKE', 'CAT', 'LMT', 'ASML', 'TSM', 'GS', 'PFE', 'SBUX', 'AMD'] 
+ """
+print(url.columns.str.strip())
 """ aapl  """
 tarih=url['Date']
+url['TSLA']=url['TSLA'].fillna(method='ffill')
+url['AMZN']=url['AMZN'].fillna(method='ffill')
+print(url.isnull().sum())
+url['Date']=pd.DataFrame(url['Date'])
+filtre=url[(url['Date'] >='2024-10-24') &(url['Date'] <='2025-10-24')]
 
-hkodu={'Close', 'MSFT', 'AMZN', 'NVDA', 'META', 'GOOG', 'TSLA', 'PEP',
-       'COST', 'AVGO', 'ADBE', 'NFLX', 'JPM', 'V', 'JNJ', 'UNH', 'XOM', 'NEE',
-       'WMT', 'NKE', 'CAT', 'LMT', 'ASML', 'TSM', 'GS', 'PFE', 'SBUX', 'AMD'}
+hkodu=['Close', 'MSFT', 'AMZN', 'NVDA', 'META', 'GOOG', 'TSLA'] 
 for hk in hkodu:
-   ilk=url[hk].iloc[0]
-   son=url[hk].iloc[-4]
+   
+   fi = url[(url['Date'] >= '2020-01-03') & (url['Date'] <= '2025-10-22')]
+
+
+   ilk=fi[hk].iloc[0]
+   son=fi[hk].iloc[-1]
    
    yüzdesel=((son -ilk)/ilk)*100
- 
-   print(f"{hk}: {yüzdesel}")
 
+   mac_50=url[hk].rolling(window=50).mean()
+   mac_200=url[hk].rolling(window=200).mean()
+   print(f"{hk}: 50 günlük: {mac_50}")
+   print(f"{hk}: 200 günlük: {mac_50}")
+   print(f"{hk}: {yüzdesel:.2f}%")
+   """ 1 yıllık """
+
+   ilkk=filtre[hk].iloc[0]
+   sonn=filtre[hk].iloc[-1]
+
+   yuz=((sonn - ilkk)/ilkk)*100
+     
+   print(f"1 yıllık {hk} değişim : {yuz:.2f}%")
    
-   sns.lineplot( x=tarih ,y=hk , data=url)
+   
+
+
+   sns.lineplot(x='Date' ,y=hk , data=url)
    plt.title(f"{hk} hisse senedi 2020-2025")
    plt.show()
-
-NVDA: 2897.17
-AVGO: 989.73
-TSLA: 1431.76
-TSM: 407.51
-AMD: 374.70
-
+    
+aa=url.corr()
+print(aa)
