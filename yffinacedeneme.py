d487361e0be5ffbf0a8e4820c458f4657d96abcd
buğra uysal <uysalbugra134@gmail.com>
2025-12-07 22:48:10 +0300

    Retrieve stock data using the yfinance API

--- yffinacedeneme.py
+++ yffinacedeneme.py
@@ -1,35 +1,55 @@
-import finnhub 
-import pandas as pd
+
+import yfinance as yf
 import requests
-from datetime import datetime 
-api="cp7rd3pr01qi8q89arpgcp7rd3pr01qi8q89arq0"
-sy=["NVDA", "AAPL", "MSFT", "AMZN", "GOOGL", "GOOG", "AVGO", "META", "TSLA",
-    "BRK.B", "LLY", "JPM", "WMT", "ORCL", "V", "XOM", "JNJ", "MA", "ABBV",
-    "PLTR", "COST", "BAC", "AMD", "PG", "HD", "GE", "CVX", "UNH", "NFLX",
-    "DIS", "KO", "PFE", "UNP", "TSM","MCD", "NKE", "SBUX", "CAT",
-    "MMM", "MRK", "ABT", "LMT", "RTX", "GLW", "HON", "UPS"]
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from numpy import info
+from google.colab import  drive
+import streamlit as st
+from datetime import datetime
+drive.mount("/content/drive")
+
+
 
 
 ecl=[]
 
-for  symbol in sy:
-    url=f"https://finnhub.io/api/v1/quote?symbol={symbol}&token={api}"
-    data=requests.get(url).json()
-    timestamp=data.get("t",None)
-    date=datetime.fromtimestamp(timestamp).strftime("%y-%m-%d") if timestamp else None
-    
-    ecl.append({
-        "symbol": symbol,
-        "Date": date,
-        "Open": data.get("o", None),
-        "Close": data.get("c",None),
-        "High": data.get("h", None),  
-        "Low": data.get("l", None),
-        "Volume": data.get("v", None),
-       
-    })
-   
+sy=["NVDA", "AAPL", "MSFT", "AMZN", "GOOGL", "GOOG", "AVGO", "META", "TSLA",
+     "LLY", "JPM", "WMT", "ORCL", "V", "XOM", "JNJ", "MA", "ABBV",
+    "PLTR", "COST", "BAC", "AMD", "PG", "HD", "GE", "CVX", "UNH", "NFLX",
+    "DIS", "KO", "PFE", "UNP", "TSM","MCD", "NKE", "SBUX", "CAT",
+    "MMM", "MRK", "ABT", "LMT", "RTX", "GLW", "HON", "UPS" ]
 
+
+
+
+
+for hisse in sy:
+ al=yf.download(hisse, start="2020-01-01", end="2025-12-03")
+
+
+
+ sns.lineplot(al["Close"])
+ plt.title(f"{hisse} 2020-2025 fiyat grafiği ")
+ plt.show()
+
+ ''' bilgiler '''
+ bıl=yf.Ticker(hisse)
+ hıs=bıl.info
+
+ ecl.append({
+    "Adı":hıs.get("longName", None),
+    "fiyat":hıs.get("currentPrice", None),
+    "kapanış":al["Close"].iloc[-1],
+    "açılış":al["Open"].iloc[-1],
+    "hacim":al["Volume"].iloc[-1],
+    "sektör":hıs.get("sector",None),
+    "endüstürü":hıs.get("industry",None),
+    "piyasa_değeri":hıs.get("marketCap",None),
+    "Website":hıs.get("website",None)
+})
 gor=pd.DataFrame(ecl)
-gor.to_excel("C:\\Users\\buğra\\Desktop\\portfoyy.xlsx")
+gor.to_excel("/content/drive/MyDrive/ecl.xlsx")
+
 
