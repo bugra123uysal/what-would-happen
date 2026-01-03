09fdcc10ba78bbf97c417e86e19805e253ed1b2d
buğra uysal <uysalbugra134@gmail.com>
2026-01-03 19:45:46 +0300

    I structured multi-year return tables to serve as groundwork for future comparative performance analysis.

--- neden oldu.py
+++ neden oldu.py
@@ -5,8 +5,8 @@ from google import genai
 import os 
 
 """ veri seti e tablo """
-sheed_id="****"
-sheed_name="ecl"
+sheed_id="***************"
+sheed_name="*****"
 aa=f"https://docs.google.com/spreadsheets/d/{sheed_id}/gviz/tq?tqx=out:csv&sheet={sheed_name}"
 
 
@@ -18,34 +18,41 @@ url["fiyat"]=pd.to_numeric(url["fiyat"], errors="coerce")
 print(url.columns)
 
 
+
 st.write("merhaba pörtföy")
 
-hk=["NVDA", "AAPL", "MSFT" , "AMZN", "GOOGL", "GOOG", "AVGO", "META", "TSLA",
-     "LLY", "JPM", "WMT", "ORCL", "V", "XOM", "JNJ", "MA", "ABBV",
-    "PLTR", "COST", "BAC", "AMD", "PG", "HD", "GE", "CVX", "UNH", "NFLX",
-    "DIS", "KO", "PFE", "UNP", "TSM","MCD", "NKE", "SBUX", "CAT",
-    "MMM", "MRK", "ABT", "LMT", "RTX", "GLW", "HON", "UPS"]
+hk=["NVDA", "AAPL", "MSFT" , "AMZN", "GOOGL", "GOOG", "AVGO", "META", "TSLA"]
 
+# anliz kısmı için
+aaiz=["SPY","QQQ","DIA","IWM","VTI","IWM","IJH","XLK","XLF","XLV","XLE","XLY"]
+
+gor_analiz=aaiz.copy()
 
 port=st.multiselect("hisse seçiniz",hk)
 pppört=[]
+analiz=[]
 
-
-
-for myprfy in port:
-
-    
+tumu=port + aaiz
+for myprfy in tumu:
     
     try:
         fıı=url.loc[url['Adı'] == myprfy, 'fiyat'].iloc[-1]
-        adet=int(st.number_input(f"{myprfy} hissesi adetini giriniz:", min_value=0))
-        sektör=url.loc[url["Adı"]==myprfy, "sektör"].iloc[0] 
+        adet=int(st.number_input(f"{myprfy} hissesi adetini giriniz:", min_value=0, key=f"adet_{myprfy}"))
+        sektör=url.loc[url["Adı"]==myprfy, "sektör"].iloc[0]
         ppee=url.loc[url["Adı"]==myprfy , "pe_oranı"].iloc[0]
         ffor=url.loc[url["Adı"]==myprfy    ,"forward_pe"].iloc[0]
         kar=url.loc[url["Adı"]== myprfy, "kar_marjı"].iloc[0]
         eeps=url.loc[url["Adı"]==myprfy,"eps"].iloc[0]
+        beta=url.loc[url["Adı"]==myprfy, "beta"].iloc[0]
 
-         
+        yıl_5=url.loc[url["Adı"]==myprfy, "5-yıl"].iloc[0] 
+        yıl_1=url.loc[url["Adı"]==myprfy,"1-yıl"].iloc[0] 
+        yıl_2=url.loc[url["Adı"]==myprfy,"2-yıl"].iloc[0] 
+        yıl_3=url.loc[url["Adı"]==myprfy,"3-yıl"].iloc[0] 
+        yıl_4=url.loc[url["Adı"]==myprfy,"4-yıl"].iloc[0] 
+
+       
+
        
         h_degeri= adet * fıı
         pppört.append({
@@ -57,52 +64,96 @@ for myprfy in port:
          "pe":ppee,
          "forward":ffor,
          "kar":kar,
-         "eps":eeps
+         "eps":eeps,
+         "beta":beta,
+         "5_yıllık_getiri":yıl_5,
+         "4_yıllık_getiri":yıl_4,
+         "3_yıllık_getiri":yıl_3,
+         "2_yıllık_getiri":yıl_2,
+         "1_yıllık_getiri":yıl_1
+      
 
-         
+    })
+        
+        # getiri
+        analiz.append({
+        "hisse":myprfy , 
+        "5_yıllık_getiri":yıl_5,
+        "4_yıllık_getiri":yıl_4,
+        "3_yıllık_getiri":yıl_3,
+        "2_yıllık_getiri":yıl_2,
+        "1_yıllık_getiri":yıl_1,
 
-    })  
+             
+             
+             }) 
+    
+        
+        
+
+
+       
 
     except:
         fıı=0
-    
 
+
+
+
+
+    
    
 my_p=pd.DataFrame(pppört)
 st.dataframe(my_p)
+
+# getiri
+aa=pd.DataFrame(analiz)
+st.dataframe(aa)
+
+
+
 if st.button("oluştur"):
+    # getiri   
     
-   
-   
+    
+    
     pörtföy= my_p["değeri"].sum()
 
     st.write(f"toplam değer: {pörtföy}")
 
     my_p["dağılım %"]=(my_p["değeri"] / pörtföy)*100
-
-    st.dataframe(my_p)
-
-    fig, ax =plt.subplots()
-    ax.pie(my_p["dağılım %"], autopct="%1.1f%%" ,labels=my_p["hisse"])
+   
     
+     # pasta grafiği dağılım
+    fig, ax =plt.subplots()
+    my_p=my_p.dropna(subset="dağılım %")
+    ax.pie(my_p["dağılım %"], autopct="%1.1f%%" ,labels=my_p["hisse"])
     st.pyplot(fig)
 
-     
-    fig, ax=plt.subplots()
-    ax.plot(url.index, url["5-yıllık"] )
+    # forward_pe grafiği
+    fig , ax=plt.subplots()
+    ax.barh(my_p["hisse"] , my_p["eps"])
     ax.grid(True)
     st.pyplot(fig)
 
+    fig , ax= plt.subplots()
+
+    ax.barh(my_p["hisse"], my_p["forward"])
+    st.pyplot(fig)
+
+
+
+
+    
     #indirme bölümü 
     st.download_button("indir (excel)", my_p.to_csv(index=True) , "pörtföy.csv")
 
-    
+
     st.balloons()
 
 # chat bot bölümü
 
-
-client=genai.Client(api_key="*****")
+client=genai.Client(api_key="AIzaSyCjDe7IloE_Pvgtsj1h2vCL0PcpHMT838c")
     
 sor=st.chat_input("sor:")
 if sor:
@@ -115,24 +166,6 @@ if sor:
   )
   st.write("cevap:", cevap.text)
   st.caption("burdaki yazılanlar yatırım tavsiyesi değildir bilgileri kontrol ediniz !!!!")
-    
 
 
 
-""" bileşik faiz hesaplama """
-
-""" anapara """
-ana_p=st.number_input("anapara",min_value=0)
-
-""" vade süresi süre """
-bekleme=st.number_input("vade süresi", min_value=0)
-""" yüzde değişimi """
-yze=[0.10,0.20,0.30,0.40,0.50,0.60,0.70,0.80,0.90,1.00]
-yüzde=st.selectbox("yüzdesel değişimi şeçiniz: " ,yze)
-
-
-if st.button("hesapla"): 
-     bileşik_f=ana_p*(1 + yüzde) ** bekleme
-     st.write("sonuç:",bileşik_f)
-
-     st.balloons()
