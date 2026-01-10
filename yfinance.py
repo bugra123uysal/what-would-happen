d3871f3c585b6d13470580589b73a761683f0c91
buğra uysal <uysalbugra134@gmail.com>
2026-01-11 00:40:37 +0300

    I implemented stock and ETF analysis with historical prices, ratios, and return metrics

--- yfinance.py
+++ yfinance.py
@@ -9,11 +9,7 @@ from datetime import datetime
 
 ecl=[]
 
-sy=["NVDA", "AAPL", "MSFT" , "AMZN", "GOOGL", "GOOG", "AVGO", "META", "TSLA",
-     "LLY", "JPM", "WMT", "ORCL", "V", "XOM", "JNJ", "MA", "ABBV",
-    "PLTR", "COST", "BAC", "AMD", "PG", "HD", "GE", "CVX", "UNH", "NFLX",
-    "DIS", "KO", "PFE", "UNP", "TSM","MCD", "NKE", "SBUX", "CAT",
-    "MMM", "MRK", "ABT", "LMT", "RTX", "GLW", "HON", "UPS"]
+sy=["NVDA", "AAPL", "MSFT" , "AMZN", "GOOGL", "GOOG", "AVGO", "META", "TSLA","SPY","QQQ","DIA","IWM","VTI","IWM","IJH","XLK","XLF","XLV","XLE","XLY"]
 
 
 """ bu günün tarih """
@@ -25,7 +21,16 @@ for hisse in sy:
  tod_day=al["Close"].iloc[0] # başlangıç
  year_20=al["Close"].iloc[-1] #   2020 fiyatı
 
- getırı_20=(al["Close"].iloc[-1] / al["Close"].iloc[0]- 1)*100 # 2020 den bu güne getiri
+
+ getırı_1=(al["Close"].iloc[-1].item() / al["Close"].iloc[-252].item() -1) * 100 #  1 yıl
+ getırı_2=(al["Close"].iloc[-1].item() / al["Close"].iloc[-252*2].item() - 1 ) * 100 #2 yıl
+ getırı_3=(al["Close"].iloc[-1].item() / al["Close"].iloc[-252*3].item() - 1 ) * 100 #3 yıl
+ getırı_4=(al["Close"].iloc[-1].item() / al["Close"].iloc[-252*4].item() - 1 ) * 100 #4 yıl
+ getırı_5=(al["Close"].iloc[-1].item() / al["Close"].iloc[-252*5].item() - 1) * 100 #  5 yıl
+
+
+
+
  plt.plot(al["Close"])
  plt.title(f"{hisse} fiyat grafiği ")
  plt.show()
@@ -42,10 +47,10 @@ for hisse in sy:
     "Tarih":datetime.today().strftime('%y-%m-%d'),
     "Adı": hisse,
     "fiyat":hıs.get("currentPrice", None),
-    "kapanış":al["Close"].iloc[-1],
-    "açılış":al["Open"].iloc[-1],
-    "5-yıllık":al["Close"].values,
-    "hacim":al["Volume"].iloc[-1].values,
+    "kapanış":al["Close"].iloc[-1].item(),
+    "açılış":al["Open"].iloc[-1].item(),
+    "5-yıllık":al["Close"],
+    "volume": int(al["Volume"].iloc[-1]),
     "pe_oranı":hıs.get("trailingPE", None),
     "forward_pe":hıs.get("forwardPE", None),
     "peg_oranı":hıs.get("pegRatio", None),
@@ -55,13 +60,29 @@ for hisse in sy:
     "endüstürü":hıs.get("industry",None),
     "piyasa_değeri":hıs.get("marketCap",None),
     "Website":hıs.get("website",None),
-    "2020_getiri": getırı_20
+    "revenue_growth": hıs.get("revenueGrowth"),
+    "earnings_growth": hıs.get("earningsGrowth"),
+    "roe": hıs.get("returnOnEquity"),
+    "roa": hıs.get("returnOnAssets"),
+    "debt_to_equity": hıs.get("debtToEquity"),
+    "current_ratio": hıs.get("currentRatio"),
+    "free_cashflow": hıs.get("freeCashflow"),
+    "operating_margin": hıs.get("operatingMargins"),
+    "52w_high": hıs.get("fiftyTwoWeekHigh"),
+    "52w_low": hıs.get("fiftyTwoWeekLow"),
+    "avg_volume": hıs.get("averageVolume"),
+    "beta": hıs.get("beta"),
+
+
+    "1-yıl": int(round(getırı_1)),
+    "2-yıl": int(round(getırı_2)),
+    "3-yıl": int(round(getırı_3)),
+    "4-yıl": int(round(getırı_4)),
+    "5-yıl": int(round(getırı_5)),
+
+
 })
 gor=pd.DataFrame(ecl)
-gor.to_excel("********")
-
-
-
-
+gor.to_excel("/content/drive/MyDrive/ecl.xlsx")
 
 
